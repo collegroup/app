9ecae741b7d568c3da415d6fdafc1da5f1733455
Arthur Colle <hello@arthurcolle.com>
2016-07-18 22:58:15 -0400

    Simple static page for now.

--- mix.exs
+++ mix.exs
@@ -37,8 +37,7 @@ defmodule App.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:exrm, "~> 1.0"}]
+     {:cowboy, "~> 1.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
